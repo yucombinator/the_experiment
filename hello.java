47f4ef355f080ddef4c8d009e2580377ab4d93b3
Dhruvan Rai <67046510+DhruvanRai@users.noreply.github.com>
2020-10-02 16:22:51 +0530

    Update hello.java
    Improve hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
  
     public static void main (String args[]) {
         
-        System.out.println("hello world!");
+        System.out.println("Hello World!");
     }
     
 }
